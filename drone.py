d7466e09fbe71f905b4e3654cfc435e97ccb12c3
abhinavbagadi05 <118181469+abhinavbagadi05@users.noreply.github.com>
2023-11-10 11:58:02 +0530

    Update drone.py

--- drone.py
+++ drone.py
@@ -18,16 +18,14 @@ def is_valid_coord(coord, height, width, height_standard, building_map):
 def heuristic(current, goal):
     return abs(current[0] - goal[0]) + abs(current[1] - goal[1])
 
-# Define the A* search algorithm with battery constraint.
-def astar_search(building_map, start, goal, height_standard, battery_capacity):
+# Define the A* search algorithm.
+def astar_search(building_map, start, goal, height_standard):
     height = len(building_map)
     width = len(building_map[0])
     open_list = [(0, start)]
     came_from = {}
     g_score = {coord: float('inf') for coord in [(x, y) for x in range(height) for y in range(width)]}
     g_score[start] = 0
-    battery = {coord: float('inf') for coord in [(x, y) for x in range(height) for y in range(width)]}
-    battery[start] = battery_capacity  # Initialize battery at start position
 
     # Set the height of the goal building to -1 in the array during runtime
     building_map[goal[0]][goal[1]] = -1
@@ -51,14 +49,11 @@ def astar_search(building_map, start, goal, height_standard, battery_capacity):
                 neighbor = (current[0] + dx, current[1] + dy)
 
                 if is_valid_coord(neighbor, height, width, height_standard, building_map):
-                    movement_cost = 1
-                    tentative_g_score = g_score[current] + movement_cost
-                    tentative_battery = battery[current] - movement_cost
+                    tentative_g_score = g_score[current] + 1
 
-                    if tentative_g_score < g_score[neighbor] and tentative_battery >= 0:
+                    if tentative_g_score < g_score[neighbor]:
                         came_from[neighbor] = current
                         g_score[neighbor] = tentative_g_score
-                        battery[neighbor] = tentative_battery
                         f_score = tentative_g_score + heuristic(neighbor, goal)
                         heapq.heappush(open_list, (f_score, neighbor))
 
@@ -66,26 +61,17 @@ def astar_search(building_map, start, goal, height_standard, battery_capacity):
 
 # Main function
 def main():
-    building_map = load_map("test.txt")
+    building_map = load_map("building_heights.txt")
     start = (0, 0)
     goal = (4, 5)  # Adjusted goal coordinates
     height_standard = 5  # Height standard for the drone
-    battery_capacity = 4  # Battery capacity for the drone
 
-    shortest_path = astar_search(start, goal, height_standard, battery_capacity)
+    shortest_path = astar_search(building_map, start, goal, height_standard)
 
     if shortest_path:
         print("Shortest Path Coordinates:")
-        remaining_battery = battery_capacity  # Initialize remaining battery
         for coord in shortest_path:
-            print("Coordinate:", coord, "Remaining Battery:", remaining_battery)
-            # Update the remaining battery based on the movement cost (assuming 1 unit of battery per step)
-            remaining_battery -= 1
-            if remaining_battery < 0 and coord != goal:
-                print("Battery not sufficient")
-                break
-        if remaining_battery > 0:
-            print("Final Remaining Battery:", remaining_battery)
+            print(coord)
     else:
         print("No valid path found")
 
